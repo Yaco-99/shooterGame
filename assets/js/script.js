dc58f93d3b489e04d393a853908c2f8a9ac93311
Yaco-99 <coopman.yannick@gmail.com>
2020-10-15 11:20:38 +0200

    v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 (function () {
   const canvas = document.getElementById("gameWindow"),
-    ctx = canvas.getContext("2d");
-  startBtn = document.getElementById("start");
+    ctx = canvas.getContext("2d"),
+    startBtn = document.getElementById("start"),
+    hardBtn = document.getElementById("hardMode"),
+    timerTarget = document.getElementById("timer"),
+    countTarget = document.getElementById("score"),
+    countMissTarget = document.getElementById("miss"),
+    scoreListTarget = document.getElementById("scoreList");
   let code,
-    xPos = 330,
+    xPos = 340,
     yPos = 530,
     ballxPos,
     ballyPos,
-    monsterX = random(780),
+    monsterX = random(760),
     monsterY = random(200),
     start = false,
-    count = 0;
+    count = 0,
+    countMiss = 0,
+    timerStart,
+    time,
+    hard,
+    playerList = [],
+    inAnimation = false;
 
   window.addEventListener("keydown", function (event) {
     if (event.key !== undefined) {
@@ -19,8 +30,12 @@
     code == " " ? shoot() : moveCannon(code);
   });
   startBtn.addEventListener("click", () => {
-    start = true;
-    startBtn.classList.add("d-none");
+    hard = false;
+    gameStart();
+  });
+  hardBtn.addEventListener("click", () => {
+    hard = true;
+    gameStart();
   });
 
   function moveCannon(event) {
@@ -36,24 +51,24 @@
           break;
         case "q":
         case "ArrowLeft":
-          xPos = xPos == 0 ? xPos : xPos - 5;
+          xPos = xPos == -10 ? xPos : xPos - 5;
           break;
         case "d":
         case "ArrowRight":
-          xPos = xPos == 730 ? xPos : xPos + 5;
+          xPos = xPos == 750 ? xPos : xPos + 5;
           break;
       }
     }
   }
 
   function shoot() {
-    ballxPos = xPos + 25;
-    ballyPos = yPos - 20;
-
-    shooting();
+    inAnimation ? "can't shoot" : shooting();
     async function shooting() {
+      ballxPos = xPos + 25;
+      ballyPos = yPos - 20;
       let exit = false;
       do {
+        inAnimation = true;
         const promise = new Promise((resolve) => {
           setTimeout(() => {
             draw();
@@ -62,16 +77,20 @@
           }, 5);
         });
         await promise;
+        ballyPos <= -20 ? (exit = true) : (exit = false);
         if (ballyPos <= monsterY + 40) {
           if (ballxPos >= monsterX - 20 && ballxPos <= monsterX + 40) {
             addScore();
             ballyPos = -50;
-            monsterX = random(780);
+            monsterX = random(760);
             monsterY = random(200);
             exit = true;
           }
         }
-      } while ((ballyPos < 0) ^ !exit);
+      } while (!exit);
+      console.log("out");
+      inAnimation = false;
+      exit ? "hit" : countMissed();
       moveCannon();
     }
   }
@@ -82,13 +101,15 @@
   let drawing = window.setInterval(function () {
     draw();
   }, 1);
+  let timing = window.setInterval(function () {
+    timer();
+  }, 1000);
 
   function draw() {
     //Cannon Ball
     if (start) {
       const cannonBall = new Image();
       cannonBall.src = "assets/img/cannonBall.svg";
-
       cannonBall.onload = function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         moveCannon();
@@ -110,16 +131,60 @@
       };
     }
   }
+
+  function timer() {
+    if (start) {
+      time = Math.floor((Date.now() - timerStart) / 1000);
+      timerTarget.innerHTML = time;
+      if (hard) {
+        if (time == 30) {
+          reset();
+          alert("You lose !");
+        }
+      }
+    }
+  }
+
   function addScore() {
     count++;
-    document.getElementById("score").innerHTML = count;
+    countTarget.innerHTML = count;
     if (count == 10) {
-      count = 0;
-      start = false;
+      displayScore();
+      reset();
       alert("You killed all of them !");
-      startBtn.classList.add("d-block");
-      startBtn.innerHTML = `Play again`;
-      document.getElementById("score").innerHTML = "0";
     }
+  }
+  function countMissed() {
+    countMiss++;
+    document.getElementById("miss").innerHTML = countMiss;
+  }
+  function displayScore() {
+    playerList.push({ time: time, miss: countMiss });
+    playerList.sort((a, b) => (a.time > b.time ? 1 : b.time > a.time ? -1 : 0));
+    console.log(playerList);
+    scoreListTarget.innerHTML = "<li>Yannick : 0.5s, 0 miss</li>";
+    for (let i = 0; i < playerList.length; i++) {
+      let newPlayer = document.createElement("li");
+      newPlayer.innerHTML = `You : ${playerList[i].time}s, ${playerList[i].miss} miss`;
+      scoreListTarget.appendChild(newPlayer);
+    }
+  }
+  function gameStart() {
+    timerStart = Date.now();
+    start = true;
+    startBtn.classList.add("d-none");
+    hardBtn.classList.add("d-none");
+  }
+  function reset() {
+    time = 0;
+    count = 0;
+    countMiss = 0;
+    start = false;
+    startBtn.classList.remove("d-none");
+    hardBtn.classList.remove("d-none");
+    startBtn.innerHTML = `Play again`;
+    countTarget.innerHTML = "0";
+    timerTarget.innerHTML = "0";
+    countMissTarget.innerHTML = "0";
   }
 })();
